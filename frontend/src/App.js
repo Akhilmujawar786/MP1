e16a466fdb30066efe8ff72fa2e4c805b874e160
Akhilmujawar786 <01fe21bcs135@kletech.ac.in>
2024-04-15 16:08:49 +0530

    changin route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './Navbar';
-import Home from './Home';
-import AQI from './AQI';
-import GreenPath from './GreenPath';
-import AboutUs from './AboutUs';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import AQI from './pages/AQI';
+import GreenPath from './pages/GreenPath';
+import AboutUs from './pages/Aboutus';
 
 const App = () => {
   return (
