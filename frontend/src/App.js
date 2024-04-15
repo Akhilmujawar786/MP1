865715f724ddafe917ac28ea9dec9e6f9b2ff058
ud4yy <ud4yworks@gmail.com>
2024-04-15 15:22:18 +0530

    before akhils

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-function App() {
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+import AQI from './pages/AQI';
+import Travel from './pages/GreenPath';
+import About from './pages/Aboutus';
+
+const App = () => {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <Router>
+      <Navbar />
+      {/* <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/aqi" component={AQI} />
+        <Route path="/travel" component={Travel} />
+        <Route path="/about" component={About} />
+      </Switch> */}
+    </Router>
   );
-}
+};
 
 export default App;
+
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,4 @@
 // Navbar.js
-
 import React from 'react';
 import { Link } from 'react-router-dom'; // Assuming you are using React Router for routing
 import './Navbar.css';
