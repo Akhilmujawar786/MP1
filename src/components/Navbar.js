3a1a45e0588b740d15cb856e064de7c86a54664d
Akhilmujawar786 <01fe21bcs135@kletech.ac.in>
2024-04-15 13:15:33 +0530

    init

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+// Navbar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom'; // Assuming you are using React Router for routing
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <nav>
+        <div className='container'>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/aqi">AQI</Link>
+        </li>
+      </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
