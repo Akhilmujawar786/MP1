9b9087528746e8c7706b735e125e995e33d410c7
ud4yy <ud4yworks@gmail.com>
2024-04-16 10:01:20 +0530

    Links added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Router and Routes
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'; // Import Router and Routes
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import AQI from './pages/AQI';
@@ -10,7 +10,12 @@ import AboutUs from './pages/Aboutus';
 const App = () => {
   return (
     <div>
+     
       <Navbar />
+      <Link to="/about">About Us</Link>
+      <Link to="/aqi">AQI</Link>
+      <Link to="/travel">GreenPath</Link>
+      <Link to="/">Home</Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aqi" element={<AQI />} />
