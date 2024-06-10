dd546892ab9402d421f1deddd34ccd99163d7273
Akhilmujawar786 <01fe21bcs135@kletech.ac.in>
2024-06-10 21:52:16 +0530

    gameoflifeadded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './Game.css';
+import GameOfLife from './GameOfLife';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <header className="App-header">
+               
+                <GameOfLife />
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
